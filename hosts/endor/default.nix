4533d07f1c370795a38aac59d3977a27dbb158bd
qqr3a <nitro5cy@outlook.com>
2025-08-30 13:31:50 +0100

    Endor hardware

--- hosts/endor/default.nix
+++ hosts/endor/default.nix
@@ -23,8 +23,6 @@
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    networking.
-
     environment = {
         variables.EDITOR = "nvim";
     };
