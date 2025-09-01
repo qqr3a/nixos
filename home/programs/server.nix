64379fe8f716f738351606af8fcca47ace9de2f9
qqr3a <nitro5cy@outlook.com>
2025-09-01 19:17:31 +0100

    server update

--- home/programs/server.nix
+++ home/programs/server.nix
@@ -7,7 +7,6 @@
         enable = true;
         user = "admin";     
         guiAddress = "0.0.0.0:8384"; 
-        configDir = "/home/admin/.config/syncthing";
         settings.gui = {
             user = "admin";
             password = "secretpw";
